4a34ed08bf8937e26c5ac1259c5edf9b354dfd66
c-biancone <chris.biancone@gmail.com>
2023-10-02 16:35:36 -0400

    initial commit

---
+++ ads1115_i2c_test/ads1115_i2c_test.ino
@@ -0,0 +1,130 @@
+#include <Adafruit_ADS1X15.h>
+#include <Wire.h>
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+int DSPIN = PA0;  // temp sensor 1-wire pin
+double temp = 0;
+
+// Pin connected to the ALERT/RDY signal for new sample notification.
+void setup(void)
+{
+  Wire.setSCL(PB_8);
+  Wire.setSDA(PB_9);
+  Wire.begin();
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+   ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+ //ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+}
+
+void loop(void)
+{
+  int16_t adc0, adc1, adc2, adc3;
+  float volts0, volts1, volts2, volts3;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  volts0 = ads.computeVolts(adc0);
+  temp = TempRead();
+  temp = temp * 0.0625; // conversion accuracy is 0.0625 / LSB
+
+  //Serial.println("-----------------------------------------------------------");
+  //Serial.print("AIN0: "); 
+  Serial.print(adc0); Serial.print(", "); 
+  Serial.print(volts0, 6); Serial.print(", "); 
+  Serial.println(temp);
+
+  delay(100);
+}
+
+/* Temp Sensor Functions */
+boolean DS18B20_Init()
+{
+  pinMode(DSPIN, OUTPUT);
+  digitalWrite(DSPIN, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(DSPIN, LOW);
+  delayMicroseconds(750);//480-960
+  digitalWrite(DSPIN, HIGH);
+  pinMode(DSPIN, INPUT);
+  int t = 0;
+  while (digitalRead(DSPIN))
+  {
+    t++;
+    if (t > 60) return false;
+    delayMicroseconds(1);
+  }
+  t = 480 - t;
+  pinMode(DSPIN, OUTPUT);
+  delayMicroseconds(t);
+  digitalWrite(DSPIN, HIGH);
+  return true;
+}
+ 
+void DS18B20_Write(byte data)
+{
+  pinMode(DSPIN, OUTPUT);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(DSPIN, LOW);
+    delayMicroseconds(10);
+    if (data & 1) digitalWrite(DSPIN, HIGH);
+    else digitalWrite(DSPIN, LOW);
+    data >>= 1;
+    delayMicroseconds(50);
+    digitalWrite(DSPIN, HIGH);
+  }
+}
+ 
+byte DS18B20_Read()
+{
+  pinMode(DSPIN, OUTPUT);
+  digitalWrite(DSPIN, HIGH);
+  delayMicroseconds(2);
+  byte data = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(DSPIN, LOW);
+    delayMicroseconds(1);
+    digitalWrite(DSPIN, HIGH);
+    pinMode(DSPIN, INPUT);
+    delayMicroseconds(5);
+    data >>= 1;
+    if (digitalRead(DSPIN)) data |= 0x80;
+    delayMicroseconds(55);
+    pinMode(DSPIN, OUTPUT);
+    digitalWrite(DSPIN, HIGH);
+  }
+  return data;
+}
+ 
+int TempRead()
+{
+  if (!DS18B20_Init()) return 0;
+  DS18B20_Write (0xCC); // Send skip ROM command
+  DS18B20_Write (0x44); // Send reading start conversion command
+  if (!DS18B20_Init()) return 0;
+  DS18B20_Write (0xCC); // Send skip ROM command
+  DS18B20_Write (0xBE); // Read the register, a total of nine bytes, the first two bytes are the conversion value
+  int temp = DS18B20_Read (); // Low byte
+  temp |= DS18B20_Read () << 8; // High byte
+  return temp;
+}
